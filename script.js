d9b3f11b6262e4aaeece3df95929983e86b8733e
SUBHANKAR GHOSH <subho2021april@gmail.com>
2024-11-08 02:54:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Function to redirect to LinkedIn profile
+function redirectToLinkedIn() {
+    window.location.href = "https://www.linkedin.com/in/subhankarghosh-data-analyst/";
+}
+
+// Event listener for LinkedIn Profile button
+document.getElementById("linkedinProfile").addEventListener("click", redirectToLinkedIn);
